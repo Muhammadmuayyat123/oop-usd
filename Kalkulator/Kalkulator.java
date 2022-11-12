65ee0707554d9fe346899a7a1d9ed1e7dc79cc99
Muhammad muayyat <100189331+Muhammadmuayyat123@users.noreply.github.com>
2022-11-12 10:50:31 +0800

    Add files via upload

---
+++ Kalkulator/Kalkulator.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package maiin;
+
+/**
+ *
+ * @author yayat
+ */
+public class Kalkulator {
+    double nilai1;
+    double nilai2;
+    
+    void isiNilai1(double x){
+        nilai1 = x;
+    }
+    
+    void isiNilai2(double y){
+        nilai2 = y;
+    }
+    
+    double tambah(){
+        return nilai1 + nilai2;
+    }
+    
+    double kurang(){
+        return nilai1 - nilai2;
+    }
+    
+    double kali(){
+        return nilai1 * nilai2;
+    }
+    
+    double bagi(){
+        return nilai1 / nilai2;
+    }
+    
+    void Eksekusi(){
+    System.out.println("Perjumlahan : " + tambah());
+    System.out.println("Pengurangan : " + kurang());
+    System.out.println("Perkalian : " + kali());
+    System.out.println("Pembagian : " + bagi());
+}
+}
---
+++ Kalkulator/Maiin.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package maiin;
+
+/**
+ *
+ * @author yayat
+ */
+public class Maiin {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Kalkulator baru = new Kalkulator();
+        baru.isiNilai1(6);
+        baru.isiNilai2(1000);
+        
+        baru.Eksekusi();
+    }
+    
+}
